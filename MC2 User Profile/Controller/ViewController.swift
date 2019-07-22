78bc09db7dc100afcb6f8200873be019d1d3365f
Thomas Pratama Putra <thomas.putra@binus.ac.id>
2019-07-22 11:47:28 +0700

    NA Final

--- MC2 User Profile/Controller/ViewController.swift
+++ MC2 User Profile/Controller/ViewController.swift
@@ -15,22 +15,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var userBowlerStatusLabel: UILabel!
     
     @IBOutlet weak var userBowlingBallWeightLabel: UILabel!
-    @IBOutlet weak var userGenderLabel: UILabel!
     @IBOutlet weak var userWeightLabel: UILabel!
     @IBOutlet weak var userHeightLabel: UILabel!
     
     @IBOutlet weak var userBowlingBallWeightView: UIView!
-    @IBOutlet weak var userGenderView: UIView!
     @IBOutlet weak var userWeightView: UIView!
     @IBOutlet weak var userHeightView: UIView!
     
     @IBOutlet weak var wViewLabel: UILabel!
     @IBOutlet weak var hViewLabel: UILabel!
     
-    @IBOutlet weak var editProfileLabel: UILabel!
+    @IBOutlet weak var editProfileBttn: UIBarButtonItem!
     
     @IBOutlet weak var bowlingBallIV: UIImageView!
-    @IBOutlet weak var genderIV: UIImageView!
+    
+    @IBOutlet weak var ballRecTitle: UILabel!
+    @IBOutlet weak var ballRecDesc: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,17 +44,13 @@ class ViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        editProfileLabel.startBlink()
         view.layoutIfNeeded()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
-        editProfileLabel.stopBlink()
     }
     
     private func setupView() {
-        let editProfileTGR = UITapGestureRecognizer(target: self, action: #selector(editProfileTapped))
-        
         view.backgroundColor = #colorLiteral(red: 0.1058823529, green: 0.1058823529, blue: 0.1960784314, alpha: 1)
         
         userPhotoProfileImageView.layer.borderWidth = 1
@@ -64,7 +61,6 @@ class ViewController: UIViewController {
         userPhotoProfileImageView.clipsToBounds = true
         
         userBowlingBallWeightView.backgroundColor = #colorLiteral(red: 0.137254902, green: 0.137254902, blue: 0.2509803922, alpha: 1)
-        userGenderView.backgroundColor = #colorLiteral(red: 0.137254902, green: 0.137254902, blue: 0.2509803922, alpha: 1)
         userWeightView.backgroundColor = #colorLiteral(red: 0.137254902, green: 0.137254902, blue: 0.2509803922, alpha: 1)
         userHeightView.backgroundColor = #colorLiteral(red: 0.137254902, green: 0.137254902, blue: 0.2509803922, alpha: 1)
         
@@ -72,35 +68,32 @@ class ViewController: UIViewController {
         userBowlerStatusLabel.textColor = .lightGray
         
         userBowlingBallWeightLabel.textColor = .white
-        userGenderLabel.textColor = .white
         userWeightLabel.textColor = .white
         userHeightLabel.textColor = .white
         
         wViewLabel.textColor = .white
         hViewLabel.textColor = .white
         
-        editProfileLabel.isUserInteractionEnabled = true
-        editProfileLabel.textColor = .lightGray
-        
-        editProfileLabel.startBlink()
-        
-        editProfileLabel.addGestureRecognizer(editProfileTGR)
-        
         bowlingBallIV.layer.borderColor = UIColor.clear.cgColor
-        genderIV.layer.borderColor = UIColor.clear.cgColor
+        
+        ballRecTitle.textColor = .white
+        ballRecDesc.textColor = .white
     }
     
     func setupLabelView(userClass: User) {
         userPhotoProfileImageView.image = userClass.userProfileImage
         userNameLabel.text = userClass.userFullName
         userBowlerStatusLabel.text = userClass.userBowlerStatus
-        userGenderLabel.text = userClass.userGender
         userBowlingBallWeightLabel.text = "\(Int(round(userClass.ballWeight()))) LB"
         userWeightLabel.text = "\(userClass.userWeight) KG"
         userHeightLabel.text = "\(userClass.userHeight) CM"
+        
+        ballRecDesc.text = "Based on the data you gave us,\nwe recommend you to use the \(Int(userClass.userBallWeight)) lb ball."
     }
     
-    @objc private func editProfileTapped() {
+    
+    @IBAction func editProfileBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: "goToEditProfile", sender: self)
     }
 }
+
